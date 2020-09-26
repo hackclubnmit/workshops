2132f54411e8c21c06a74754ca51fa982f957c55
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2020-09-26 23:01:48 +0530

    Update Loader.js

--- 012-Sep-27-2020/src/Loader.js
+++ 012-Sep-27-2020/src/Loader.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
-import { OrbitControls, HTML } from "drei";
+// import { OrbitControls, HTML } from "drei";
 
 
 extend({ OrbitControls });
